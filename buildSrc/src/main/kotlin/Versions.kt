03e8e40058c112ffcbb8588a018026475958cf3a
Daniele Bottillo <dbottillo@gmail.com>
2021-11-06 10:51:32 +0000

    Dependencies update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,29 +1,29 @@
 object Versions {
     const val gradle = "4.0.1"
-    const val androidGradlePlugin = "7.0.0" // update also build.gradle.kts in buildSrc
+    const val androidGradlePlugin = "7.0.3" // update also build.gradle.kts in buildSrc
 
-    const val kotlin = "1.5.21"  // update also build.gradle.kts in buildSrc
-    const val coroutines = "1.5.1"
+    const val kotlin = "1.5.31"  // update also build.gradle.kts in buildSrc
+    const val coroutines = "1.5.2"
 
     object AndroidX {
-        const val core = "1.6.0"
+        const val core = "1.7.0"
         const val cardview = "1.0.0"
         const val recyclerview = "1.2.1"
         const val compat = "1.3.1"
         const val preference = "1.1.1"
         const val navigation = "2.3.5"
-        const val lifecycle = "2.4.0-alpha03"
+        const val lifecycle = "2.4.0"
         const val lifecycleExtensions = "2.2.0"
         const val dataStore = "1.0.0"
     }
 
     const val material = "1.4.0"
-    const val dagger = "2.38.1" // update also build.gradle.kts in buildSrc
+    const val dagger = "2.40" // update also build.gradle.kts in buildSrc
     const val hilt = "1.0.0"
     const val leakCanary = "2.7"
-    const val workManager = "2.6.0-rc01"
+    const val workManager = "2.7.0"
 
-    const val constraint_layout = "2.1.0"
+    const val constraint_layout = "2.1.1"
 
     object Retrofit {
         const val core = "2.9.0"
@@ -32,8 +32,8 @@ object Versions {
     const val moshi = "1.12.0"
 
     object OkHttp {
-        const val core = "4.9.1"
-        const val logging = "4.9.1"
+        const val core = "4.9.2"
+        const val logging = "4.9.2"
     }
 
     // testing
@@ -41,10 +41,9 @@ object Versions {
     const val mockito = "3.11.2"
     const val mockito_kotlin = "3.2.0"
     const val mockito_android = "3.8.0"
-    const val junit = "5.7.2"
-    const val robolectric = "4.3.1"
+    const val junit = "5.8.1"
     const val truth = "1.1.3"
-    const val lint = "30.0.0"
+    const val lint = "30.0.3"
 
-    const val ktlint = "0.42.0"
+    const val ktlint = "0.43.0"
 }
