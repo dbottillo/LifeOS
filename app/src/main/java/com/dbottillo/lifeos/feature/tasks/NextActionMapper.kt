eb72b65d27a51043bf764db20f06789a6810a61a
Daniele <dbottillo@gmail.com>
2024-01-23 08:12:54 +0000

    Handle date and date with time due

--- app/src/main/java/com/dbottillo/lifeos/feature/tasks/NextActionMapper.kt
+++ app/src/main/java/com/dbottillo/lifeos/feature/tasks/NextActionMapper.kt
@@ -1,13 +1,15 @@
 package com.dbottillo.lifeos.feature.tasks
 
 import com.dbottillo.lifeos.db.NotionEntry
+import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.Locale
 import javax.inject.Inject
 
 class NextActionMapper @Inject constructor() {
 
-    private val inputDateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+    private val inputDateAndTimeFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault())
+    private val inputDateFormatter = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
     private val dateFormatter = SimpleDateFormat("dd/MM HH:mm", Locale.getDefault())
 
     fun map(input: List<NotionEntry>): List<NextAction> {
@@ -15,14 +17,20 @@ class NextActionMapper @Inject constructor() {
             val name = entry.title ?: "No title"
             val emoji = entry.emoji ?: ""
             val text = emoji + name
+            val date: String? = try {
+                entry.startDate?.let { inputDateAndTimeFormatter.parse(it) }?.let {
+                    dateFormatter.format(it)
+                }
+            } catch (_: Exception) {
+                entry.startDate?.let { inputDateFormatter.parse(it) }?.let {
+                    dateFormatter.format(it)
+                }
+            }
             NextAction(
                 text = text,
                 url = entry.url,
                 color = entry.color ?: "",
-                due = entry.startDate?.let {
-                    inputDateFormatter.parse(it)
-                    ?.let { it1 -> dateFormatter.format(it1) }
-                } ?: ""
+                due = date ?: ""
             )
         }
     }
