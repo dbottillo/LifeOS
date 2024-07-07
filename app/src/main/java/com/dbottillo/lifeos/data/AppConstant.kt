519f7cf803cd5b517c50564463eed3fcc8c7fbdc
Daniele <dbottillo@gmail.com>
2024-07-07 20:37:00 +0100

    Bump dependencies and update goals block id

--- app/src/main/java/com/dbottillo/lifeos/data/AppConstant.kt
+++ app/src/main/java/com/dbottillo/lifeos/data/AppConstant.kt
@@ -5,5 +5,5 @@ object AppConstant {
     const val NOTION_ARTICLE_PAGE_URL = "https://www.notion.so/dbottillo/ef1963ca16574555874f5c3dc2523b61?v=e0eb4cd891f9456e89da25314d772523"
     const val GTD_ONE_DATABASE_ID = "1ecf1aad5b75430686cb91676942e5f1"
     const val ARTICLES_DATABASE_ID = "ef1963ca16574555874f5c3dc2523b61"
-    const val GOALS_BLOCK_ID = "75680ca457924684b2a7cb5841939a4e"
+    const val GOALS_BLOCK_ID = "e14be719bc3f45618e1dad0cb67b1c81"
 }
