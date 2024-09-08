dcf9a00278ea91f9afdee659f11737a02cd1cb47
Daniele <dbottillo@gmail.com>
2024-09-08 19:40:16 +0100

    Add @Transaction annotation in dao

--- app/src/main/java/com/dbottillo/lifeos/db/NotionEntryDao.kt
+++ app/src/main/java/com/dbottillo/lifeos/db/NotionEntryDao.kt
@@ -10,24 +10,36 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface NotionEntryDao {
 
+    // The return value includes a POJO with a @Relation.
+    // It is usually desired to annotate this method with @Transaction to avoid possibility
+    // of inconsistent results between the POJO and its relations.
+    // See https://developer.android.com/reference/androidx/room/Transaction.html for details.
+
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE type = 'alert'")
     fun getNextActions(): Flow<List<NotionEntryWithParent>>
 
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE status = 'Blocked'")
     fun getBlocked(): Flow<List<NotionEntryWithParent>>
 
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE type = 'Project'")
     fun getProjects(): Flow<List<NotionEntryWithParent>>
 
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE type = 'Area'")
     fun getAreas(): Flow<List<NotionEntryWithParent>>
 
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE type = 'Idea'")
     fun getIdeas(): Flow<List<NotionEntryWithParent>>
 
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE type = 'Resource'")
     fun getResources(): Flow<List<NotionEntryWithParent>>
 
+    @Transaction
     @Query("SELECT * FROM notionEntry WHERE type = 'Goal'")
     fun getGoals(): Flow<List<NotionEntryWithParent>>
 
